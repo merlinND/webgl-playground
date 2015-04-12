9fabc2ceaf6fbad620ad5c112a25281f1350908e
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-04-12 12:47:12 +0200

    Allow coordinates to be specified in pixels

--- js/app.js
+++ js/app.js
@@ -14,6 +14,13 @@ var setupProgram = function(gl, vertexShader, fragmentShader) {
   return program;
 }
 
+var setResolution = function(gl, program, canvasId) {
+  var canvas = document.getElementById(canvasId);
+
+  var resolutionAttribute = gl.getUniformLocation(program, 'u_resolution');
+  gl.uniform2f(resolutionAttribute, canvas.width, canvas.height);
+}
+
 // Allocate and fill a GL buffer with vertices
 var makeTrianglesBuffer = function(gl, coordinates, attribute) {
   var buffer = gl.createBuffer();
@@ -33,17 +40,20 @@ var makeTrianglesBuffer = function(gl, coordinates, attribute) {
 }
 
 var main = function() {
-  var gl = getContext('canvas');
+  var canvasId = 'canvas';
+  var gl = getContext(canvasId);
   var program = setupProgram(gl, '2d-vertex-shader', '2d-fragment-shader');
+
+  setResolution(gl, program, canvasId);
 
   var positionAttribute = gl.getAttribLocation(program, 'a_position');
   var verticesCoordinates = [
-      -1.0, -1.0,
-       1.0, -1.0,
-      -1.0,  1.0,
-      -1.0,  1.0,
-       1.0, -1.0,
-       1.0,  1.0
+      10, 10,
+      40, 10,
+      10, 40,
+      10, 40,
+      40, 10,
+      40, 40
     ];
   makeTrianglesBuffer(gl, verticesCoordinates, positionAttribute);
 
