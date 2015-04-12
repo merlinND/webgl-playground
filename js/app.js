e75620b984db495e90fb8ec14bc753f9d533c290
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-04-12 13:04:18 +0200

    Refactor & draw random rectangles on screen

--- js/app.js
+++ js/app.js
@@ -21,44 +21,72 @@ var setResolution = function(gl, program, canvasId) {
   gl.uniform2f(resolutionAttribute, canvas.width, canvas.height);
 }
 
+var setColor = function(gl, program, color) {
+  var colorAttribute = gl.getUniformLocation(program, 'u_color');
+  gl.uniform4fv(colorAttribute, color);
+}
+
 // Allocate and fill a GL buffer with vertices
-var makeTrianglesBuffer = function(gl, coordinates, attribute) {
+var makeBufferForRectangle = function(gl, attribute) {
+  var nTriangles = 2;
+
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(
-      gl.ARRAY_BUFFER,
-      new Float32Array(coordinates),
-      gl.STATIC_DRAW
-    );
-
-  var nTriangles = (coordinates.length / 6);
 
   gl.enableVertexAttribArray(attribute);
   gl.vertexAttribPointer(attribute, nTriangles, gl.FLOAT, false, 0, 0);
 
   return buffer;
 }
+var fillRectangleBuffer = function(gl, coordinates) {
+  gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array(coordinates),
+      gl.STATIC_DRAW
+    );
+}
+
+var drawRectangle = function(gl, x, y, width, height) {
+  var verticesCoordinates = [
+      x,         y,
+      x,         y + height,
+      x + width, y,
+      x + width, y + height,
+      x        , y + height,
+      x + width, y,
+    ];
+
+  // Draw triangles
+  fillRectangleBuffer(gl, verticesCoordinates);
+  gl.drawArrays(gl.TRIANGLES, 0, verticesCoordinates.length / 2);
+}
+
+var drawRandomRectangle = function(gl, program, maxWidth, maxHeight) {
+  var color = [Math.random(), Math.random(), Math.random(), 1];
+  var x = Math.random() * maxWidth,
+      y = Math.random() * maxHeight,
+      width = Math.random() * (maxWidth - x),
+      height = Math.random() * (maxHeight - y);
+
+  setColor(gl, program, color);
+  drawRectangle(gl, x, y, width, height, color);
+}
 
 var main = function() {
   var canvasId = 'canvas';
+  var canvas = document.getElementById(canvasId);
+
   var gl = getContext(canvasId);
   var program = setupProgram(gl, '2d-vertex-shader', '2d-fragment-shader');
 
   setResolution(gl, program, canvasId);
 
   var positionAttribute = gl.getAttribLocation(program, 'a_position');
-  var verticesCoordinates = [
-      10, 10,
-      40, 10,
-      10, 40,
-      10, 40,
-      40, 10,
-      40, 40
-    ];
-  makeTrianglesBuffer(gl, verticesCoordinates, positionAttribute);
+  makeBufferForRectangle(gl, positionAttribute);
 
-  // Draw triangles
-  gl.drawArrays(gl.TRIANGLES, 0, verticesCoordinates.length / 2);
+  for(var i = 0; i < 30; i += 1) {
+    drawRandomRectangle(gl, program, canvas.width, canvas.height);
+  }
 }
 
 main();
